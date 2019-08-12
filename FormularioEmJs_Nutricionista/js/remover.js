4100b2698a3d53a1eec018a7ffd64dfc1532f878
rapha1908 <rapha.santi@gmai.com>
2019-08-12 19:51:20 +0100

    animação de remoção comcluida

--- FormularioEmJs_Nutricionista/js/remover.js
+++ FormularioEmJs_Nutricionista/js/remover.js
@@ -2,6 +2,12 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event) {
-     event.target.parentNode.remove();
+  	event.target.parentElement.classList.add("fadeOut");
+
+	
+	setTimeout(function(){
+		event.target.parentNode.remove();
+
+	},500);	
 });
 	
